612520d118f0381e18b6e7c2ee8b10ba2229075d
mkosmala <mkosmala@pgs-soft.com>
2026-01-11 17:36:44 +0100

    Dependency injection

--- NetWorthTracker/App.xaml.cs
+++ NetWorthTracker/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using NetWorthTracker.Database.Repositories;
+using NetWorthTracker.Database.Repositories.Interfaces;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +12,48 @@ namespace NetWorthTracker
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider _serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // Configure Logging
+            services.AddLogging();
+
+            // Register Services
+            //services.AddSingleton<IUserService, UserService>();
+            services.AddSingleton<IUserRepository, UserRepository>();
+            services.AddSingleton<IEntryRepository, EntryRepository>();
+            services.AddSingleton<IAssetRepository, AssetRepository>();
+            services.AddSingleton<IDebtRepository, DebtRepository>();
+            services.AddSingleton<IAssetDefinitionRepository, AssetDefinitionRepository>();
+            services.AddSingleton<IDebtDefinitionRepository, DebtDefinitionRepository>();
+
+            // Register ViewModels
+            services.AddSingleton<IMainViewModel, MainViewModel>();
+
+            // Register Views
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void OnExit(object sender, ExitEventArgs e)
+        {
+            // Dispose of services if needed
+            if (_serviceProvider is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
     }
 
 }
--- NetWorthTracker/MainWindow.xaml.cs
+++ NetWorthTracker/MainWindow.xaml.cs
@@ -16,9 +16,10 @@ namespace NetWorthTracker
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(IMainViewModel viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
         }
     }
 }
