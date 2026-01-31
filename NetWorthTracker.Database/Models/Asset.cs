a294e93d4fc1a736bbc991984cf084c7ad486b15
ogendogen <kosmos500@interia.pl>
2026-01-31 21:06:55 +0100

    Properties live update on EntryWindow

--- NetWorthTracker.Database/Models/Asset.cs
+++ NetWorthTracker.Database/Models/Asset.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace NetWorthTracker.Database.Models;
 
-public class Asset
+public class Asset : INotifyPropertyChanged
 {
     public int Id { get; set; }
-    public required string Name { get; set; } = string.Empty;
+    
+    private string _name = string.Empty;
+    public required string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            OnPropertyChanged();
+        }
+    }
+    
     public int EntryId { get; set; }
-    public decimal Value { get; set; } = 0;
+    
+    private decimal _value = 0;
+    public decimal Value
+    {
+        get => _value;
+        set
+        {
+            _value = value;
+            OnPropertyChanged();
+        }
+    }
+    
     public virtual Entry Entry { get; set; } = new Entry();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
--- NetWorthTracker/Entry/EntryWindowViewModel.cs
+++ NetWorthTracker/Entry/EntryWindowViewModel.cs
@@ -5,6 +5,8 @@ using NetWorthTracker.Database.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Text;
 
 namespace NetWorthTracker.Entry;
@@ -15,16 +17,100 @@ public interface IEntryWindowViewModel
     User User { get; set; }
 }
 
-public class EntryWindowViewModel : IEntryWindowViewModel
+public class EntryWindowViewModel : IEntryWindowViewModel, INotifyPropertyChanged
 {
     private readonly IAssetRepository _assetRepository;
     private readonly IDebtRepository _debtRepository;
     private readonly IDefinitionRepository _definitionRepository;
 
-    public ObservableCollection<Asset> Assets { get; set; }
-    public ObservableCollection<Debt> Debts { get; set; }
+    private ObservableCollection<Asset> _assets;
+    public ObservableCollection<Asset> Assets
+    {
+        get => _assets;
+        set
+        {
+            if (_assets != null)
+                _assets.CollectionChanged -= OnAssetsChanged;
+            _assets = value;
+            if (_assets != null)
+                _assets.CollectionChanged += OnAssetsChanged;
+            OnPropertyChanged(nameof(Assets));
+            OnPropertyChanged(nameof(AssetsSum));
+            OnPropertyChanged(nameof(TotalSum));
+        }
+    }
+
+    private ObservableCollection<Debt> _debts;
+    public ObservableCollection<Debt> Debts
+    {
+        get => _debts;
+        set
+        {
+            if (_debts != null)
+                _debts.CollectionChanged -= OnDebtsChanged;
+            _debts = value;
+            if (_debts != null)
+                _debts.CollectionChanged += OnDebtsChanged;
+            OnPropertyChanged(nameof(Debts));
+            OnPropertyChanged(nameof(DebtsSum));
+            OnPropertyChanged(nameof(TotalSum));
+        }
+    }
+
+    private void OnAssetsChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.OldItems != null)
+            foreach (Asset item in e.OldItems)
+                item.PropertyChanged -= OnAssetPropertyChanged;
+        if (e.NewItems != null)
+            foreach (Asset item in e.NewItems)
+                item.PropertyChanged += OnAssetPropertyChanged;
+        OnPropertyChanged(nameof(AssetsSum));
+        OnPropertyChanged(nameof(TotalSum));
+    }
+
+    private void OnDebtsChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.OldItems != null)
+            foreach (Debt item in e.OldItems)
+                item.PropertyChanged -= OnDebtPropertyChanged;
+        if (e.NewItems != null)
+            foreach (Debt item in e.NewItems)
+                item.PropertyChanged += OnDebtPropertyChanged;
+        OnPropertyChanged(nameof(DebtsSum));
+        OnPropertyChanged(nameof(TotalSum));
+    }
+
+    private void OnAssetPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(Asset.Value))
+        {
+            OnPropertyChanged(nameof(AssetsSum));
+            OnPropertyChanged(nameof(TotalSum));
+        }
+    }
+
+    private void OnDebtPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(Debt.Value))
+        {
+            OnPropertyChanged(nameof(DebtsSum));
+            OnPropertyChanged(nameof(TotalSum));
+        }
+    }
+
     public Database.Models.Entry SelectedEntry { get; set; }
+    public string AssetsSum => $"Suma: {Assets?.Sum(x => x.Value).ToString("0.00") ?? "0.00"}";
+    public string DebtsSum => $"Suma: {Debts?.Sum(x => x.Value).ToString("0.00") ?? "0.00"}";
+    public string TotalSum => $"Całkowita suma: {(Assets?.Sum(x => x.Value) ?? 0) - (Debts?.Sum(x => x.Value) ?? 0):0.00}";
     public User User { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
     public EntryWindowViewModel(IDefinitionRepository definitionRepository)
     {
         _definitionRepository = definitionRepository;
@@ -32,17 +118,25 @@ public class EntryWindowViewModel : IEntryWindowViewModel
 
     public async void LoadData()
     {
-        var assetsDefinitions = await _definitionRepository.GetDefinitionsByUserId(User.Id, DefinitionType.Asset); // todo: userid
-        Assets = new ObservableCollection<Asset>(assetsDefinitions.Value.Select(assetDefinition => new Asset()
+        var assetsDefinitions = await _definitionRepository.GetDefinitionsByUserId(User.Id, DefinitionType.Asset);
+        var assetsList = assetsDefinitions.Value.Select(assetDefinition => new Asset()
         {
             Name = assetDefinition.Name,
-        }));
+        }).ToList();
+        
+        Assets = new ObservableCollection<Asset>(assetsList);
+        foreach (var asset in Assets)
+            asset.PropertyChanged += OnAssetPropertyChanged;
 
-        var debtsDefinitions = await _definitionRepository.GetDefinitionsByUserId(User.Id, DefinitionType.Debt); // todo: userid
-        Debts = new ObservableCollection<Debt>(debtsDefinitions.Value.Select(debtDefinition => new Debt()
+        var debtsDefinitions = await _definitionRepository.GetDefinitionsByUserId(User.Id, DefinitionType.Debt);
+        var debtsList = debtsDefinitions.Value.Select(debtDefinition => new Debt()
         {
             Name = debtDefinition.Name,
-        }));
+        }).ToList();
+        
+        Debts = new ObservableCollection<Debt>(debtsList);
+        foreach (var debt in Debts)
+            debt.PropertyChanged += OnDebtPropertyChanged;
     }
 
     public ISeries[] Series { get; set; } =
