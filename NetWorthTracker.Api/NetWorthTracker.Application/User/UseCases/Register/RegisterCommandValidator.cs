64b7baf8a1f57a3540a20294f79bb6e7a38b3d4e
mkupczyk1999 <mkupczyk1999@gmail.com>
2026-09-10 21:47:56 +0200

    Use MaximumLength instead of Length for validation purposes

--- NetWorthTracker.Api/NetWorthTracker.Application/User/UseCases/Register/RegisterCommandValidator.cs
+++ NetWorthTracker.Api/NetWorthTracker.Application/User/UseCases/Register/RegisterCommandValidator.cs
@@ -8,7 +8,7 @@ public sealed class RegisterCommandValidator : AbstractValidator<RegisterCommand
     {
         RuleFor(x => x.Username)
             .NotEmpty().WithMessage("Provided username cannot be empty")
-            .Length(1, 32).WithMessage("Provided login cannot be longer than 32 characters");
+            .MaximumLength(32).WithMessage("Provided login cannot be longer than 32 characters");
 
         RuleFor(x => x.Password)
             .NotEmpty().WithMessage("Provided password cannot be empty");
@@ -16,6 +16,6 @@ public sealed class RegisterCommandValidator : AbstractValidator<RegisterCommand
         RuleFor(x => x.Email)
             .NotEmpty().WithMessage("Provided email cannot be empty")
             .EmailAddress().WithMessage("Provided email is not a valid email address")
-            .Length(1, 320).WithMessage("Provided email address cannot be longer than 320 characters");
+            .MaximumLength(320).WithMessage("Provided email address cannot be longer than 320 characters");
     }
 }
