ac25cc1cdc98ac7f1a1b1bf9f477f4a6444eb460
mkupczyk1999 <mkupczyk1999@gmail.com>
2026-09-10 21:44:30 +0200

    Add min/max length rules for login and email to RegisterCommandValidator

--- NetWorthTracker.Api/NetWorthTracker.Application/User/UseCases/Register/RegisterCommandValidator.cs
+++ NetWorthTracker.Api/NetWorthTracker.Application/User/UseCases/Register/RegisterCommandValidator.cs
@@ -6,10 +6,16 @@ public sealed class RegisterCommandValidator : AbstractValidator<RegisterCommand
 {
     public RegisterCommandValidator()
     {
-        RuleFor(x => x.Username).NotEmpty().WithMessage("Provided username cannot be empty");
-        RuleFor(x => x.Password).NotEmpty().WithMessage("Provided password cannot be empty");
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage("Provided username cannot be empty")
+            .Length(1, 32).WithMessage("Provided login cannot be longer than 32 characters");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Provided password cannot be empty");
+
         RuleFor(x => x.Email)
             .NotEmpty().WithMessage("Provided email cannot be empty")
-            .EmailAddress().WithMessage("Provided email is not a valid email address");
+            .EmailAddress().WithMessage("Provided email is not a valid email address")
+            .Length(1, 320).WithMessage("Provided email address cannot be longer than 320 characters");
     }
 }
