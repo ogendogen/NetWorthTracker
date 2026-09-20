bdf2647bff06de495c41e464ff49c06dbc176c60
ogendogen <ogendogen@gmail.com>
2026-09-20 17:06:46 +0200

    Email verification 2

--- NetWorthTracker.Api/NetWorthTracker.Application/User/UseCases/ConfirmEmail/ConfirmEmailCommandHandler.cs
+++ NetWorthTracker.Api/NetWorthTracker.Application/User/UseCases/ConfirmEmail/ConfirmEmailCommandHandler.cs
@@ -1,23 +1,33 @@
 ﻿using FluentResults;
 using Microsoft.Extensions.Logging;
+using NetWorthTracker.Application.Authentication.Interfaces;
 using NetWorthTracker.Application.Common.Handlers;
 using NetWorthTracker.Application.User.Models.ConfirmEmail;
-using NetWorthTracker.Application.User.Models.Login;
-using NetWorthTracker.Application.User.UseCases.Login;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace NetWorthTracker.Application.User.UseCases.ConfirmEmail;
 
 public class ConfirmEmailCommandHandler : BaseRequestHandler<ConfirmEmailCommand, ConfirmEmailResponse>
 {
-    public ConfirmEmailCommandHandler(IServiceProvider? services, ILogger<BaseRequestHandler<ConfirmEmailCommand, ConfirmEmailResponse>> logger) : base(services, logger)
+    private readonly ITokenService _tokenService;
+
+    public ConfirmEmailCommandHandler(
+        IServiceProvider? services,
+        ILogger<BaseRequestHandler<ConfirmEmailCommand, ConfirmEmailResponse>> logger,
+        ITokenService tokenService)
+        : base(services, logger)
     {
+        _tokenService = tokenService;
     }
 
     protected override ValueTask<Result<ConfirmEmailResponse>> Handler(ConfirmEmailCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var result = _tokenService.ValidateEmailToken(request.Token);
+
+        if (result.IsFailed)
+        {
+            return ValueTask.FromResult(Result.Fail<ConfirmEmailResponse>("Invalid token"));
+        }
+
+        return ValueTask.FromResult(Result.Ok(new ConfirmEmailResponse(IsEmailConfirmed: true)));
     }
 }
